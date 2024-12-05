0cd02960e9083f35e2c6c5e588a8c87b8e83a4a7
Abrar Shahriar <abrarshahriar9410@gmail.com>
2024-12-05 18:22:25 +0600

    Get & Post operation included

--- index.js
+++ index.js
@@ -26,12 +26,94 @@ async function run() {
   try {
     // Connect the client to the server	
     await client.connect();
+
+    const database = client.db("Chill-Gamer");
+    const reviewCollection = database.collection("reviews");
+    const gameCollection = database.collection("games");
+
+    // GET requests
+    app.get('/reviews', async (req, res) => {
+        const cursor = reviewCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    
+    app.get('/games', async (req, res) => {
+        const cursor = gameCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/reviews/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await reviewCollection.findOne(query);
+        res.send(result);
+    })
+    
+    app.get('/games/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await gameCollection.findOne(query);
+        res.send(result);
+    })
+
+    // POST requests
+    app.post('/reviews', async (req, res) => {
+        const newReview = req.body;
+        const result = await reviewCollection.insertOne(newReview);
+        res.send(result);
+    })
+    
+    app.post('/games', async (req, res) => {
+        const newGame = req.body;
+        const result = await gameCollection.insertOne(newGame);
+        res.send(result);
+    })
+
+    // PATCH requests
+    app.patch('/reviews/:id', async (req, res) => {
+        const id = req.params.id;
+        const updatedReview = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+            $set: {
+                title: updatedReview.title,
+                coverImg: updatedReview.coverImg,
+                description: updatedReview.description,
+                genre: updatedReview.genre,
+                publishedYear: updatedReview.publishedYear,
+                rating: updatedReview.rating,
+                userName: updatedReview.userName,
+                userEmail: updatedReview.userEmail
+            }
+        }
+        const result = await reviewCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+    })
+    
+    app.patch('/games/:id', async (req, res) => {
+        const id = req.params.id;
+        const newReview = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const game = await gameCollection.findOne(filter); 
+        const reviewsArray = game.reviews;
+        const updatedReviewsArray = [...reviewsArray,newReview];
+        const updatedDoc = {
+            $set: {
+                reviews: updatedReviewsArray
+            }
+        }
+        const result = await gameCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
